069f160f29a33851d4705f3566cfc7fc2c4b5743
Tyler Mustard <tjm.0084@gmail.com>
2018-04-03 10:41:38 -0600

    all tests passing

--- drills/version-1/car.js
+++ drills/version-1/car.js
@@ -1,3 +1,23 @@
 //Your code here.
-
+class Car {
+  constructor() {
+    this.gallons = 0;
+    this.milesDriven = 0;
+    this.tripsTaken = [];
+  }
+  fill(gals) {
+    this.gallons += gals;
+  }
+  drive(miles) {
+    this.gallons -= miles / 10;
+    this.milesDriven += miles;
+    this.tripsTaken.push(miles + " miles");
+  }
+  odometer() {
+    return this.milesDriven;
+  }
+  trips() {
+    return this.tripsTaken;
+  }
+}
 module.exports = Car;
