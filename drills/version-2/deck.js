e7b1bf89855ae506bb7b895af8074033a10f82a7
Tyler Mustard <tjm.0084@gmail.com>
2018-04-03 11:32:09 -0600

    all tests passing

--- drills/version-2/deck.js
+++ drills/version-2/deck.js
@@ -1,4 +1,35 @@
 // Your code here
+class Card {
+  constructor(rank, suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+}
+
+class Deck {
+  constructor() {
+    this.cards = [];
+  }
+  add(card) {
+    this.cards.push(card);
+  }
+  filter(suit) {
+    let filteredDeck = [];
+    for (var i = 0; i < this.cards.length; i++) {
+      if (this.cards[i].suit === suit) {
+        filteredDeck.push(this.cards[i]);
+      }
+    }
+    return filteredDeck;
+  }
+  populate(newCards) {
+    for (var i = 0; i < newCards.length; i++) {
+      var newCard = new Card(newCards[i][0], newCards[i][1]);
+      this.cards.push(newCard);
+    }
+    return this.cards;
+  }
+}
 
 module.exports = {
   Card,
