2fa994cdf90c2aa039d7ec8986674148a4c88651
Tyler Mustard <tjm.0084@gmail.com>
2018-04-03 13:52:35 -0600

    4 tests passing

--- drills/version-3/directory.js
+++ drills/version-3/directory.js
@@ -1,3 +1,33 @@
 // Your code here
+class Directory {
+  constructor(name) {
+    this.name = name;
+    this.list = [];
+    this.lengths = [];
+    this.files = [];
+  }
+  ls() {
+    return this.list;
+  }
+  write(fileName, fileText) {
+    this.list.unshift(fileName);
+    this.lengths.unshift(`${fileName} - ${fileText.length}`);
+    this.files.unshift(fileText);
+  }
+  ls_la() {
+    return this.lengths;
+  }
+  cat(fileName) {
+    for (var i = 0; i < this.list.length; i++) {
+      if (fileName === this.list[i]) {
+        return this.files[i];
+      }
+    }
+  }
+  mv(oldName, newName) {}
+}
 
 module.exports = Directory;
+
+// for (var i = 0; i < this.files.length; i++) {
+//   return this.files[i].value.length;
